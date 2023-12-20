c444487c8d17b30313b188394c1b08efa4f09580
uttam das <utu.das@gmail.com>
2023-12-21 00:18:23 +0100

    More exceptions handling added: MethodArgumentTypeMismatchException, NoResourceFoundException, HttpRequestMethodNotSupportedException

--- src/main/java/ss/todo/util/TodoExceptionHandler.java
+++ src/main/java/ss/todo/util/TodoExceptionHandler.java
@@ -3,8 +3,11 @@ package ss.todo.util;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class TodoExceptionHandler {
@@ -13,6 +16,24 @@ public class TodoExceptionHandler {
   public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
       
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+      
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = NoResourceFoundException.class)
+  public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException ex) {
+      
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+  public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+      
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
   }
 
   @ExceptionHandler(value = NullPointerException.class)
